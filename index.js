053ad9559695d68eebd78e6137413e51fe675634
Alicia <alicia@ringover.com>
2020-09-10 15:54:36 +0200

    setting up server with ssl

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express')
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('HELLO WORLD')
+var options = {
+	key: fs.readFileSync('/etc/ssl/server.key'),
+	cert:fs.readFileSync('/etc/ssl/server.crt'),
+};
+
+
+var server = https.createServer(options, app).listen(8080, () => {
+	console.log('Listening on port 8080');
 });
 
-app.listen(8080, () => {
-	console.log('Listening on port 8080')
+
+app.get('/', (req, res) => {
+	res.send('HELLO WORLD');
+	console.log('someone make a get request');
 });
